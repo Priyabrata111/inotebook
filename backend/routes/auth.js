6c23aa0c0561945154fe842cba18c9e2180950df
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-09-02 20:11:31 +0530

    Adding validation before storing data to DB

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-
-  res.send("Hello");
-});
+router.post(
+  "/",
+  [
+    body("name", "Please Enter a valid name with minimum 3 character").isLength(
+      { min: 3 }
+    ),
+    body("email", "Please Enter a valid email").isEmail(),
+    body(
+      "password",
+      "Please Enter a strong password with minimum length of 5"
+    ).isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ error: result.array() });
+    }
+    res.send(req.body);
+  }
+);
 
 module.exports = router;

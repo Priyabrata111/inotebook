f36efcf3d0418cbe41ef6cd97d8ba64ea068707e
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-09-09 23:39:58 +0530

    Fetching all notes details

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middlewares/fetchuser");
+const Note = require("../models/Notes");
 
-router.get("/", (req, res) => {
-  res.json([]);
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  const note = await Note.find({ user: res.user.id });
+
+  res.json({ note });
 });
 
 module.exports = router;

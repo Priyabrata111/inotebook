f9539669c643293205f7c774a8bceba610da37ed
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-09-10 23:47:13 +0530

    Added addnote router to create note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const fetchuser = require("../middlewares/fetchuser");
-const Note = require("../models/Notes");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
+// ROUTER 1:fetech all notes of an user
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   const note = await Note.find({ user: req.user.id });
 
   res.json({ note });
 });
+
+//ROUTER 2 : createnotes
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body(
+      "title",
+      "Please Enter a valid title with minimum 3 character"
+    ).isLength({ min: 3 }),
+    body(
+      "description",
+      "Please Enter a valid description with 5 character"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ error: result.array() });
+    }
+
+    try {
+      const { title, description, tag } = req.body;
+      const note = new Note({ title, description, tag, user: req.user.id });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 
 module.exports = router;

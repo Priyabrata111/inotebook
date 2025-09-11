f63dcf24815144a3f31071de612ec9fd31986d0b
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-09-11 23:50:20 +0530

    Update Notes from api

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,14 +4,14 @@ const fetchuser = require("../middlewares/fetchuser");
 const Note = require("../models/Note");
 const { body, validationResult } = require("express-validator");
 
-// ROUTER 1:fetech all notes of an user
+// ROUTER 1:fetech all notes of an user : api/notes/fetchallnotes
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   const note = await Note.find({ user: req.user.id });
 
   res.json({ note });
 });
 
-//ROUTER 2 : createnotes
+//ROUTER 2 : createnotes : api/notes/addnote
 router.post(
   "/addnote",
   fetchuser,
@@ -42,5 +42,30 @@ router.post(
     }
   }
 );
+
+//ROUTER 3 : updateNotes : api/notes/updatenote/:id
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  let newNote = {};
+  if (title) newNote.title = title;
+  if (description) newNote.description = description;
+  if (tag) newNote.tag = tag;
+
+  const note = await Note.findById(req.params.id);
+  if (!note) {
+    res.status(400).send("Note with provided Id Not found");
+  }
+
+  if (note.user.toString() !== req.body.user) {
+    res.status(401).send("Unauthorized user trying to access the note");
+  }
+
+  const updateNote = await Note.findByIdAndUpdate(
+    req.param.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.json(updateNote);
+});
 
 module.exports = router;

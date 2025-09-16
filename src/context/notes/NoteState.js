3fc7b5f26f4e882d4f2580dcf109617680d4fb60
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-09-16 23:31:59 +0530

    creating contextApi for managing Notes state

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,7 @@
+import noteContext from "./noteContext";
+
+const NoteState = (props) => {
+  return <noteContext.Provider>{Props.child}</noteContext.Provider>;
+};
+
+export default NoteState;
---
+++ src/context/notes/noteContext.js
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+
+const noteContext = useContext();
+
+export default noteContext;

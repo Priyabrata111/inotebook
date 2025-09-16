3026dd4827e469a56b9126e2e1282eca7dd8f0b6
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-09-16 23:42:26 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,7 +1,13 @@
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  return <noteContext.Provider>{Props.child}</noteContext.Provider>;
+  const state = {
+    name: "Priyabrata",
+    Organization: "KPIT",
+  };
+  return (
+    <noteContext.Provider value={state}>{Props.child}</noteContext.Provider>
+  );
 };
 
 export default NoteState;

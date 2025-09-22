b6aebf236acd6daa720ddd5b7be7f593fb54af4c
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-09-22 22:53:28 +0530

    Update NoteItem.js

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function NoteItem(props) {
   const { note } = props;
   return (
-    <div className="col-md-3">
+    <div className="col-md-4">
       <div className="card my-3">
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>

fc999608306453bfe1f4ee437cba4aa5b0023fee
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-10-05 17:20:25 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function Alert(props) {
+export default function Alret(props) {
   return (
-    <div>
-      <div className="alert alert-success" role="alert">
-        {props.message}
-      </div>
+    <div style={{ height: "50px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong>{props.alert.type}</strong> : {props.alert.msg}
+        </div>
+      )}
     </div>
   );
 }
-
-export default Alert;
